df97cff644ac04fc293e51c144d35a5f167e0908
Kanns103 <jackh826@icloud.com>
2022-04-23 02:47:11 +0100

    Updated cv

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 function myFunction() {
-	var dots = document.getElementById("readmore");
-	var moreText = document.getElementById("more");
-	var btnText = document.getElementById("but");
+	const dots = document.getElementById("readmore");
+	const moreText = document.getElementById("more");
+	const btnText = document.getElementById("but");
 
 	if (dots.style.display === "none") {
 		dots.style.display = "inline";
@@ -12,4 +12,163 @@ function myFunction() {
 		btnText.innerHTML = "Read less"; 
 		moreText.style.display = "inline";
 	}
+}
+
+/*
+function darkmode() {
+	const title = document.getElementById('title');
+	const rightback = document.getElementById('rightback');
+	rightback.style.transition = 'all 1s';
+	title.style.transition = 'all 1s';
+	document.body.style.transition = 'all 1s';
+
+	if(document.body.style.backgroundColor == 'rgb(76, 76, 76)') {
+		title.style.color = 'white';
+		rightback.style.color = 'rgb(76, 76, 76)';
+		document.body.style.backgroundColor = 'white';
+	} else {
+		rightback.style.color = 'white';
+		title.style.color = 'rgb(76, 76, 76)';
+		document.body.style.backgroundColor = 'rgb(76, 76, 76)';
+	}
+}
+*/
+
+function expandcode() {
+	const codeexpand = document.getElementById('codeexpand');
+	const codeexpandbutton = document.getElementById('codeexpandbutton');
+
+	if(codeexpand.style.display == 'block') {
+		codeexpandbutton.innerHTML = 'Code';
+		codeexpand.style.display = 'none';
+		codeexpandbutton.style.backgroundColor = 'white';
+	} else {
+		codeexpandbutton.style.backgroundColor = 'lightCoral';
+		codeexpandbutton.innerHTML = 'Collapse';
+		codeexpand.style.display = 'block';
+	}
+}
+
+function expandmusic() {
+	const musicexpand = document.getElementById('musicexpand');
+	const musicexpandbutton = document.getElementById('musicexpandbutton');
+
+	if(musicexpand.style.display == 'block') {
+		musicexpandbutton.innerHTML = 'Music';
+		musicexpand.style.display = 'none';
+		musicexpandbutton.style.backgroundColor = 'white';
+	} else {
+		musicexpandbutton.style.backgroundColor = 'lightCoral';
+		musicexpandbutton.innerHTML = 'Collapse';
+		musicexpand.style.display = 'block';
+	}
+}
+
+function expandgym() {
+	const gymexpand = document.getElementById('gymexpand');
+	const gymexpandbutton = document.getElementById('gymexpandbutton');
+
+	if(gymexpand.style.display == 'block') {
+		gymexpandbutton.innerHTML = 'Gym';
+		gymexpand.style.display = 'none';
+		gymexpandbutton.style.backgroundColor = 'white';
+	} else {
+		gymexpandbutton.style.backgroundColor = 'lightCoral';
+		gymexpandbutton.innerHTML = 'Collapse';
+		gymexpand.style.display = 'block';
+	}
+}
+
+function expandgcse() {
+	const gcseexpand = document.getElementById('gcseexpand');
+	const gcseexpandbutton = document.getElementById('gcseexpandbutton');
+
+	if(gcseexpand.style.display == 'block') {
+		gcseexpandbutton.innerHTML = 'GCSEs';
+		gcseexpand.style.display = 'none';
+		gcseexpandbutton.style.backgroundColor = 'white';
+	} else {
+		gcseexpandbutton.style.backgroundColor = 'lightCoral';
+		gcseexpandbutton.innerHTML = 'Collapse';
+		gcseexpand.style.display = 'block';
+	}
+}
+
+function expandalevel() {
+	const alevelexpand = document.getElementById('alevelexpand');
+	const alevelexpandbutton = document.getElementById('alevelexpandbutton');
+
+	if(alevelexpand.style.display == 'block') {
+		alevelexpandbutton.innerHTML = 'A-Levels';
+		alevelexpand.style.display = 'none';
+		alevelexpandbutton.style.backgroundColor = 'white';
+	} else {
+		alevelexpandbutton.style.backgroundColor = 'lightCoral';
+		alevelexpandbutton.innerHTML = 'Collapse';
+		alevelexpand.style.display = 'block';
+	}
+}
+
+function expanddegree() {
+	const degreeeexpand = document.getElementById('degreeexpand');
+	const degreeexpandbutton = document.getElementById('degreeexpandbutton');
+
+	if(degreeexpand.style.display == 'block') {
+		degreeexpandbutton.innerHTML = 'Degrees';
+		degreeexpand.style.display = 'none';
+		degreeexpandbutton.style.backgroundColor = 'white';
+	} else {
+		degreeexpandbutton.style.backgroundColor = 'lightCoral';
+		degreeexpandbutton.innerHTML = 'Collapse';
+		degreeexpand.style.display = 'block';
+	}
+}
+
+function expandmusicacademy() {
+	const musicacademyexpand = document.getElementById('musicacademyexpand');
+	const musicacademyexpandbutton = document.getElementById('musicacademyexpandbutton');
+
+	if(musicacademyexpand.style.display == 'block') {
+		musicacademyexpandbutton.innerHTML = 'Music Academy';
+		musicacademyexpand.style.display = 'none';
+		musicacademyexpandbutton.style.backgroundColor = 'white';
+	} else {
+		musicacademyexpandbutton.style.backgroundColor = 'lightCoral';
+		musicacademyexpandbutton.innerHTML = 'Collapse';
+		musicacademyexpand.style.display = 'block';
+	}
+}
+
+function expandentrepreneurship() {
+	const entrepreneurshipexpand = document.getElementById('entrepreneurshipexpand');
+	const entrepreneurshipexpandbutton = document.getElementById('entrepreneurshipexpandbutton');
+
+	if(entrepreneurshipexpand.style.display == 'block') {
+		entrepreneurshipexpandbutton.innerHTML = 'Entrepreneurship';
+		entrepreneurshipexpand.style.display = 'none';
+		entrepreneurshipexpandbutton.style.backgroundColor = 'white';
+	} else {
+		entrepreneurshipexpandbutton.style.backgroundColor = 'lightCoral';
+		entrepreneurshipexpandbutton.innerHTML = 'Collapse';
+		entrepreneurshipexpand.style.display = 'block';
+	}
+}
+
+function expandfreeguitarlessons() {
+	const freeguitarlessonsexpand = document.getElementById('freeguitarlessonsexpand');
+	const freeguitarlessonsexpandbutton = document.getElementById('freeguitarlessonsexpandbutton');
+
+	if(freeguitarlessonsexpand.style.display == 'block') {
+		freeguitarlessonsexpandbutton.innerHTML = 'Free guitar lessons';
+		freeguitarlessonsexpand.style.display = 'none';
+		freeguitarlessonsexpandbutton.style.backgroundColor = 'white';
+	} else {
+		freeguitarlessonsexpandbutton.style.backgroundColor = 'lightCoral';
+		freeguitarlessonsexpandbutton.innerHTML = 'Collapse';
+		freeguitarlessonsexpand.style.display = 'block';
+	}
+}
+
+function email() {
+	window.location.href = 'mailto:jwhughes03@yahoo.com';
 }
